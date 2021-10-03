6b8834d3d4bea9301ff5462ea167d400cedcc529
Michael Winser <mike.winser@gmail.com>
2021-10-03 13:58:56 -0700

    css grid for size changes

--- index.js
+++ index.js
@@ -1,26 +1,48 @@
+const contentsArray = [
+    {
+        title: "Environment",
+        img: "https://images.unsplash.com/photo-1631949351744-8bd2f28c8200?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80"
+    },{
+        title: "Schedule",
+        img: "https://images.unsplash.com/photo-1633249422795-1f3e5fe0a843?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80"
+    },{
+        title: "Routine",
+        img: "https://images.unsplash.com/photo-1633248611990-ed00611b7c97?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80"
+    },{
+        title: "Daytime",
+        img: "https://images.unsplash.com/photo-1633179893959-eaa23b7fe9ea?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1374&q=80"
+    },{
+        title: "Insomnia",
+        img: "https://images.unsplash.com/photo-1633181066775-438ec783b6ba?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1357&q=80"
+    }
+]
+
 const accordion = document.getElementById("accordion-container")
 
 for (var i = 1;i<=5;i++){
     window['panel'+i] = accordion.appendChild(document.createElement('div'))
     window['panel'+i].setAttribute('class', 'panel')
     window['panel'+i].setAttribute('id', 'panel'+ i)
-    window['panel'+i].appendChild(document.createElement('p')).innerText = 'Panel ' + i
-    window['panel'+i].appendChild(document.createElement('img'))
+
 }
 
 const allPanels = document.querySelectorAll('.panel')
 
-const imgArray = [
-    "https://images.unsplash.com/photo-1631949351744-8bd2f28c8200?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80",
-    "https://images.unsplash.com/photo-1633249422795-1f3e5fe0a843?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80",
-    "https://images.unsplash.com/photo-1633248611990-ed00611b7c97?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80",
-    "https://images.unsplash.com/photo-1633179893959-eaa23b7fe9ea?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1374&q=80",
-    "https://images.unsplash.com/photo-1633181066775-438ec783b6ba?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1357&q=80"
-]
+
 
 allPanels.forEach((panel, i)=> {
     panel.addEventListener('click', (event)=> TogglePanelSize(event))
-    panel.children[1].setAttribute('src', imgArray[i] )
+    var title = panel.appendChild(document.createElement('p'))
+    title.innerText = contentsArray[i].title
+    title.classList.add('title')
+
+    var image = panel.appendChild(document.createElement('img'))
+    image.setAttribute('src', contentsArray[i].img )
+
+    var summary = panel.appendChild(document.createElement('p'))
+    summary.innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam varius quam eros, semper dapibus ante accumsan sagittis. Fusce dignissim ullamcorper leo, sit amet dignissim nisi tempus ac. Vivamus iaculis enim sit amet semper molestie. Nulla facilisi. Donec consectetur, nulla sit amet lacinia pulvinar, leo justo fringilla neque, ut elementum nisl ex ornare nunc. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque egestas ipsum dui. Donec sapien ipsum, sollicitudin convallis feugiat at, rutrum eget leo. Nunc venenatis ex vitae lacinia tempor. Vestibulum suscipit lectus non odio fringilla malesuada. Duis vitae semper felis. Suspendisse potenti. Ut ornare vitae erat sed efficitur. Nulla a magna eget lectus tempor faucibus."
+    summary.classList.add('summary')
+    
 }) 
 
 
@@ -36,20 +58,20 @@ function TogglePanelSize (event) {
     if (panelToChange.classList.contains("full")){
         FullMenuReturn()
     }
-    else if (panelToChange.classList.contains("active")) { //hide all other panels
+    else if (panelToChange.classList.contains("active")) { //go to fullscreen
         for (panel of allPanels){   
-            if (panelToChange!=panel) {
+            if (panelToChange!=panel) { //hide other panels
                 panel.classList.add("shrink")
                 HidePanel(panel)
             } 
             else {
-                panel.classList.add("full")
+                panel.classList.add("full") //expand this panel
                 //load full content
             }
 
         }
                 
-    } else { //expand this one, reduce others
+    } else { //expand panel to large(active), reduce others to small
         for (panel of allPanels){
             panelToChange==panel 
                 ?   panel.classList.add("active") 
