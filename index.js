fcecb0e9d5d191e0ab972686729896db3e907e76
Michael Winser <mike.winser@gmail.com>
2021-10-03 13:01:51 -0700

    added images

--- index.js
+++ index.js
@@ -4,14 +4,24 @@ for (var i = 1;i<=5;i++){
     window['panel'+i] = accordion.appendChild(document.createElement('div'))
     window['panel'+i].setAttribute('class', 'panel')
     window['panel'+i].setAttribute('id', 'panel'+ i)
-    window['panel'+i].innerHTML = 'Panel ' + i
+    window['panel'+i].appendChild(document.createElement('p')).innerText = 'Panel ' + i
+    window['panel'+i].appendChild(document.createElement('img'))
 }
 
 const allPanels = document.querySelectorAll('.panel')
 
-for (panel of allPanels) {
-    panel.addEventListener('click', (event)=>TogglePanelSize(event.target))
-}
+const imgArray = [
+    "https://images.unsplash.com/photo-1631949351744-8bd2f28c8200?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80",
+    "https://images.unsplash.com/photo-1633249422795-1f3e5fe0a843?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80",
+    "https://images.unsplash.com/photo-1633248611990-ed00611b7c97?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80",
+    "https://images.unsplash.com/photo-1633179893959-eaa23b7fe9ea?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1374&q=80",
+    "https://images.unsplash.com/photo-1633181066775-438ec783b6ba?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1357&q=80"
+]
+
+allPanels.forEach((panel, i)=> {
+    panel.addEventListener('click', (event)=> TogglePanelSize(event))
+    panel.children[1].setAttribute('src', imgArray[i] )
+}) 
 
 
 
@@ -20,7 +30,9 @@ for (panel of allPanels) {
 
 
 
-function TogglePanelSize (panelToChange) {
+
+function TogglePanelSize (event) {
+    var panelToChange = event.path.filter(item=>item.classList ? item.classList.contains('panel') : null)[0]
     if (panelToChange.classList.contains("full")){
         FullMenuReturn()
     }
