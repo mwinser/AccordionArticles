605009fcde6dd93f3a8efaaba049fb71420c700a
Michael Winser <mike.winser@gmail.com>
2021-10-08 17:37:40 -0700

    added freshwater panel

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 const contentsArray = [
-    {
-        title: "Desert",
-        img: "https://images.unsplash.com/photo-1473580044384-7ba9967e16a0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80",
-        text: "The common defining feature among desert ecosystems is low precipitation, generally less than 25 centimeters, or 10 inches, per year. Not all deserts are hot – desert ecosystems can exist from the tropics to the arctic, but regardless of latitude, deserts are often windy. Some deserts contain sand dunes, while others feature mostly rock. Vegetation is sparse or nonexistent, and any animal species, such as insects, reptiles and birds, must be highly adapted to the dry conditions."
-    },{
+{
         title: "Forest",
         img: "https://images.unsplash.com/photo-1448375240586-882707db888b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
         text: "Forest ecosystems are classified according to their climate type as tropical, temperate or boreal. In the tropics, rainforest ecosystems contain more diverse flora and fauna than ecosystems in any other region on earth. In these warm, moisture-laden environments, trees grow tall and foliage is lush and dense, with species inhabiting the forest floor all the way up to the canopy. In temperate zones, forest ecosystems may be deciduous, coniferous or oftentimes a mixture of both, in which some trees shed their leaves each fall, while others remain evergreen year-round. In the far north, just south of the Arctic, boreal forests – also known as taiga – feature abundant coniferous trees."
@@ -12,9 +8,17 @@ const contentsArray = [
         img: "https://images.unsplash.com/photo-1593629674805-d3e08c061eaf?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1974&q=80",
         text: "Different types of grassland ecosystems can be found in prairies, savannas and steppes. Grassland ecosystems are typically found in tropical or temperate regions, although they can exist in colder areas as well, as is the case with the well-known Siberian steppe. Grasslands share the common climactic characteristic of semi-aridity. Trees are sparse or nonexistent, but flowers may be interspersed with the grasses. Grasslands provide an ideal environment for grazing animals."
     },{
+        title: "Desert",
+        img: "https://images.unsplash.com/photo-1473580044384-7ba9967e16a0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80",
+        text: "The common defining feature among desert ecosystems is low precipitation, generally less than 25 centimeters, or 10 inches, per year. Not all deserts are hot – desert ecosystems can exist from the tropics to the arctic, but regardless of latitude, deserts are often windy. Some deserts contain sand dunes, while others feature mostly rock. Vegetation is sparse or nonexistent, and any animal species, such as insects, reptiles and birds, must be highly adapted to the dry conditions."
+    },{
         title: "Tundra",
         img: "https://images.unsplash.com/photo-1586196935120-f7780c76746e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2083&q=80",
         text: "As with deserts, a harsh environment characterizes ecosystems in the tundra. In the snow-covered, windswept, treeless tundra, the soil may be frozen year-round, a condition known as permafrost. During the brief spring and summer, snows melt, producing shallow ponds which attract migrating waterfowl. Lichens and small flowers may become visible during this time of year. The term “tundra” most commonly denotes polar areas, but at lower latitudes, tundra-like communities known as alpine tundra may be found at high elevations."
+    },{
+        title: "Freshwater",
+        img: "https://images.unsplash.com/photo-1559310589-2673bfe16970?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2070&q=80",
+        text: "Freshwater ecosystems can be found in streams, rivers, springs, ponds, lakes, bogs and freshwater swamps. They are subdivided into two classes: those in which the water is nearly stationary, such as ponds, and those in which the water flows, such as creeks. Freshwater ecosystems are home to more than just fish: algae, plankton, insects, amphibians and underwater plants also inhabit them."
     },{
         title: "Marine",
         img: "https://images.unsplash.com/photo-1561623002-b6648b879b15?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1974&q=80",
@@ -24,7 +28,7 @@ const contentsArray = [
 
 const accordion = document.getElementById("accordion-container")
 
-for (var i = 1;i<=5;i++){
+for (var i = 1;i<=contentsArray.length;i++){
     window['panel'+i] = accordion.appendChild(document.createElement('div'))
     window['panel'+i].setAttribute('class', 'panel')
     window['panel'+i].setAttribute('id', 'panel'+ i)
